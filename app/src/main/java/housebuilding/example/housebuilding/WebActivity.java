536657e71bd186d69109c4cb0641e3b3fe970a12
levanthai18ct111 <levanthai170920@gmail.com>
2021-12-16 20:58:07 +0700

    backup - planB

---
+++ app/src/main/java/housebuilding/example/housebuilding/WebActivity.java
@@ -0,0 +1,54 @@
+package housebuilding.example.housebuilding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class WebActivity extends AppCompatActivity {
+
+    WebView mWebview;
+    private String link;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+
+        if(getIntent() != null && getIntent().getExtras() != null){
+            link = getIntent().getExtras().getString("link");
+        }
+
+
+        mWebview  = new WebView(this);
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        final Activity activity = this;
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
+
+        Log.d("link_get",link.toString());
+        mWebview .loadUrl("https://"+link);
+        setContentView(mWebview );
+    }
+}
---
+++ app/src/main/java/housebuilding/example/housebuilding/WebModel.java
@@ -0,0 +1,45 @@
+package housebuilding.example.housebuilding;
+
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+
+public class WebModel {
+    private String title,url;
+    private int img;
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public WebModel() {
+    }
+
+    public WebModel(String title, String url, int img) {
+        this.title = title;
+        this.url = url;
+        this.img = img;
+    }
+
+
+
+}
